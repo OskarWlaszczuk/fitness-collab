a96d473a90b1a8b6e02b84f23f21129a7dd53ad5
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-17 15:19:41 +0200

    create initial database schema for fitness collaboration app. Added users, trainers and specializations entity tables and trainer_specializations relational table

---
+++ server/fitness_collab_db.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE fitness_collab_db;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    nickname VARCHAR(30) UNIQUE NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    surname VARCHAR(30) NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE trainers (
+    id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(), 
+    description VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+ 
+CREATE TABLE specializations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE trainer_specializations (
+    trainer_id INT NOT NULL,
+    specialization_id INT NOT NULL,
+    PRIMARY KEY (trainer_id, specialization_id),
+    FOREIGN KEY (trainer_id) REFERENCES trainers (id) ON DELETE CASCADE,
+    FOREIGN KEY (specialization_id) REFERENCES specializations (id) ON DELETE CASCADE
+);
