9cca6daa147fe1d14c458775fb893446eff8bb03
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-22 12:41:25 +0200

    Ordered functions by their logic layers (helpers, services, controllers). Seperated functions logics by layers responsibilities to create code with cleaner architecture. Renamed initializeJWTsSession fn to generareJWTs and splitted its logic: moved refresh token cookie assign to controllers layer and moved UPDATE db query to startUserTokenSession service. Moved registering new user buisness logic to registerUser service that uses transaction.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,9 +5,11 @@ import bcrypt from "bcrypt";
 
 config();
 //czy poniższe funkcje to servicy?
-const generateAccessToken = (userPayload) => {
+
+//helpers
+const generateAccessToken = (tokenPayload) => {
     const accessToken = jwt.sign(
-        userPayload,
+        tokenPayload,
         process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn: process.env.ACCESS_TTL,
@@ -16,8 +18,7 @@ const generateAccessToken = (userPayload) => {
 
     return accessToken;
 };
-
-const initializeJWTsSession = async ({ response, user, role }) => {
+const generareJWTs = async ({ user, role }) => {
     try {
         const tokenPayload = {
             userId: user.id,
@@ -25,6 +26,7 @@ const initializeJWTsSession = async ({ response, user, role }) => {
         };
 
         const accessToken = generateAccessToken(tokenPayload);
+
         const refreshToken = jwt.sign(
             tokenPayload,
             process.env.REFRESH_TOKEN_SECRET,
@@ -33,84 +35,98 @@ const initializeJWTsSession = async ({ response, user, role }) => {
             }
         );
 
-        response.cookie(
-            process.env.COOKIE_NAME,
-            refreshToken,
-            {
-                httpOnly: true,
-                secure: false,
-                sameSite: "lax",
-                path: process.env.COOKIE_PATH,
-                maxAge: 7 * 24 * 3600 * 1000,
-            });
-
         const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
 
-        await pool.query(
-            "UPDATE users SET refresh_token_hash = $1 WHERE id = $2",
-            [hashedRefreshToken, user.id]
-        );
-
-        return accessToken;
+        return { accessToken, refreshToken, hashedRefreshToken };
     } catch (error) {
-        console.log("Error in initializeJWTsSession function:", error);
         throw error
     }
 };
 
-export const login = async (request, response) => {
-    const { email, password, role } = request.body;
-    console.log(`logging ${role} ${email}...`);
+//services
+const startUserTokenSession = async ({ client, userId, hashedRefreshToken }) => {
+    await client.query(
+        "UPDATE users SET refresh_token_hash = $1 WHERE id = $2",
+        [hashedRefreshToken, userId]
+    );
+};
+
+const registerUser = async ({ userData, role }) => {
+    const client = await pool.connect();
 
     try {
-        const { rows: roleRows } = await pool.query('SELECT * FROM roles WHERE name = $1', [role]);
+        await client.query('BEGIN');
 
-        if (!roleRows.length) {
-            console.log('Role does not exist');
+        const { rows: userRows } = await client.query(
+            "INSERT INTO users (email, name, surname, nickname, password_hash) \
+            VALUES ($1, $2, $3, $4, $5) \
+            RETURNING id, name, surname, email, nickname, created_at",
+            [...userData]
+        );
+        const user = userRows[0];
+        console.log("Inserted new user:", user);
 
-            return response
-                .status(400)
-                .json({ success: false, message: "Role doesn't exist" });
+        if (role === "trainer") {
+            console.log(`registering new ${role}...`);
+
+            await client.query('INSERT INTO trainers (user_id) VALUES ($1)', [user.id]);
         }
 
-        const { rows: userRows } = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-        if (!userRows.length) {
-            console.log('Email does not exist');
+        if (role === "client") {
+            console.log(`registering new ${role}...`);
+
+            await client.query('INSERT INTO clients (user_id) VALUES ($1)', [user.id]);
+        }
+
+        const { accessToken, refreshToken, hashedRefreshToken } = generareJWTs({ role, user });
+
+        await startUserTokenSession({ client, hashedRefreshToken, userId: user.id })
+
+        await client.query('COMMIT');
+
+        return {
+            accessToken,
+            refreshToken,
+            user,
+        };
+    } catch (error) {
+        await client.query('ROLLBACK');
+        throw error;
+    } finally {
+        client.release();
+    }
+};
+
+export const findRoleByName = async (roleName) => {
+    const { rows: roleRows } = await pool.query('SELECT * FROM roles WHERE name = $1', [roleName]);
+    return roleRows;
+};
+
+//controllers
+export const getTrainerName = async (request, response) => {
+    const { trainerId } = request.body;
+
+    try {
+        //pobranie user_id
+        const { rows: trainerRows } = await pool.query('SELECT user_id FROM trainers WHERE id = $1', [trainerId]);
+
+        if (!trainerRows.length) {
+            console.log('Trainer does not exist');
 
             return response
                 .status(409)
-                .json({ message: "Email does not registered" });
+                .json({ message: "Trainer does not exist" });
         }
 
-        const { password_hash, refresh_token_hash, ...user } = userRows[0];
-        console.log(user);
+        const { user_id: userId } = trainerRows[0];
 
-        const isValidPassword = await bcrypt.compare(password, password_hash);
-        if (!isValidPassword) {
-            return response
-                .status(401)
-                .json({ success: false, message: "Invalid password" });
-        }
+        const { rows: userRows } = await pool.query('SELECT name FROM users WHERE id = $1', [userId]);
+        const { name } = userRows[0];
 
-        const accessToken = await initializeJWTsSession({ user, role, response });
-
-        console.log("user logined");
-        response
-            .status(201)
-            .json({
-                success: true,
-                data: {
-                    user,
-                    role,
-                    accessToken,
-                }
-            });
-
+        return response.status(200).json({ name });
     } catch (error) {
-        console.error("error in login controller", error.message);
-        response
-            .status(500)
-            .json({ success: false, message: "Internal server error" });
+        console.log(error);
+        return response
     }
 };
 
@@ -119,8 +135,9 @@ export const register = async (request, response) => {
         const { email, name, surname, nickname, password, role } = request.body;
         console.log(`Registering user ${name}...`);
 
-        const existingRoles = await pool.query('SELECT * FROM roles WHERE name = $1', [role]);
-        if (!existingRoles.rows.length) {
+        //service sprawdzający czy rola istnieje
+        const roleRows = findRoleByName(role);
+        if (!roleRows.length) {
             console.log("Role does not exist");
 
             return response
@@ -144,36 +161,22 @@ export const register = async (request, response) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const { rows: userRows } = await pool.query(
-            "INSERT INTO users (email, name, surname, nickname, password_hash) \
-            VALUES ($1, $2, $3, $4, $5) \
-            RETURNING id, name, surname, email, nickname, created_at",
-            [email, name, surname, nickname, hashedPassword]
+        const { accessToken, user, refreshToken } = registerUser({
+            userData: [email, name, surname, nickname, hashedPassword],
+            role,
+        });
+
+        response.cookie(
+            process.env.COOKIE_NAME,
+            refreshToken,
+            {
+                httpOnly: true,
+                secure: false,
+                sameSite: "lax",
+                path: process.env.COOKIE_PATH,
+                maxAge: 7 * 24 * 3600 * 1000,
+            }
         );
-        console.log("Added new user");
-
-        const user = userRows[0];
-        console.log(user);
-
-        if (role === "trainer") {
-            console.log(`registering new ${role}...`);
-
-            await pool.query(
-                'INSERT INTO trainers (user_id) VALUES ($1)',
-                [user.id]
-            );
-        }
-
-        if (role === "client") {
-            console.log(`registering new ${role}...`);
-
-            await pool.query(
-                'INSERT INTO clients (user_id) VALUES ($1)',
-                [user.id]
-            );
-        }
-
-        const accessToken = await initializeJWTsSession({ user, role, response });
 
         return response
             .status(200)
@@ -195,3 +198,70 @@ export const register = async (request, response) => {
     }
 };
 
+export const login = async (request, response) => {
+    const { email, password, role } = request.body;
+    console.log(`logging ${role} ${email}...`);
+
+    try {
+        const roleRows = findRoleByName(role);
+
+        if (!roleRows.length) {
+            console.log('Role does not exist');
+
+            return response
+                .status(400)
+                .json({ success: false, message: "Role doesn't exist" });
+        }
+
+        const { rows: userRows } = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (!userRows.length) {
+            console.log('Email does not exist');
+
+            return response
+                .status(409)
+                .json({ message: "Email does not registered" });
+        }
+
+        const { password_hash, refresh_token_hash, ...user } = userRows[0];
+        console.log(user);
+
+        const isValidPassword = await bcrypt.compare(password, password_hash);
+        if (!isValidPassword) {
+            //czy w sytuacji, gdy isValidPassword jest false powinienem obsłużyć ten błąd tutaj w warunku bloku try i tuaj zwrócić odpowiedź, czy rzuci wyjątek i obsłużyć go catch?
+            return response
+                .status(401)
+                .json({ success: false, message: "Invalid password" });
+        }
+
+        const { accessToken, refreshToken, hashedRefreshToken } = await generareJWTs({ user, role });
+        await startUserTokenSession({ client: pool, hashedRefreshToken, userId: user.id })
+
+        response.cookie(
+            process.env.COOKIE_NAME,
+            refreshToken,
+            {
+                httpOnly: true,
+                secure: false,
+                sameSite: "lax",
+                path: process.env.COOKIE_PATH,
+                maxAge: 7 * 24 * 3600 * 1000,
+            }
+        );
+
+        response
+            .status(201)
+            .json({
+                success: true,
+                data: {
+                    user,
+                    role,
+                    accessToken,
+                }
+            });
+    } catch (error) {
+        console.error("error in login controller", error.message);
+        response
+            .status(500)
+            .json({ success: false, message: "Internal server error" });
+    }
+};
