cdcdb0005acb71867d12550fe22de6a7d4d4a1a3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-23 12:07:39 +0200

    Refactor globalErrorHandler to separate development and production error responses

--- server/controllers/errorControllers.js
+++ server/controllers/errorControllers.js
@@ -1,10 +1,45 @@
+import { config } from "dotenv";
+
+config();
+
+const devErrors = (error, response) => {
+    response.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+        stack: error.stack,
+        error,
+    });
+};
+
+const prodErrors = (error, response) => {
+    error.isOperational ?
+        response.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+        }) :
+        response.status(500).json({
+            status: "error",
+            message: "Internal server error occurred. Please try again later.",
+        });
+};
+
 export const globalErrorHandler = (error, request, response, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || "error";
 
-    console.log("Error caught in globalErrorHandler:", error);
+    const nodeEnv = process.env.NODE_ENV;
 
-    response
-        .status(error.statusCode)
-        .json({ message: error.message, status: error.statusCode });
+    switch (nodeEnv) {
+        case "development":
+            console.log("Error caught in globalErrorHandler:", error);
+            devErrors(error, response);
+            break;
+
+        case "production":
+            prodErrors(error, response);
+            break;
+
+        default:
+            break;
+    }
 };
