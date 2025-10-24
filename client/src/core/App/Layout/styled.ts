0f854eb892b9c24b75b0391f15ffb6d12396599c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-24 17:36:09 +0200

    Add styled components for Layout structure and main content area

---
+++ client/src/core/App/Layout/styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledLayout = styled.div`
+  display: grid;
+  grid-template-columns: 64px 1fr;
+  grid-template-rows: auto 1fr 1fr;
+  grid-template-areas:
+    "search search search"
+    "nav main main"
+    "nav main main";
+  height: 100%;
+  grid-gap: 8px;
+  padding: 10px;
+`;
+
+export const MainContent = styled.main``;
