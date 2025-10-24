e728966465b6b5d5dfbefe6a1fd1034ffc40c449
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-24 17:11:31 +0200

    Added global styles, wrapped components into ThemeProvider,ReactQueryDevtools and QueryClientProvider in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-
+import { App } from './core/App/index.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { GlobalStyles } from './core/GlobalStyles.ts';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './core/theme.ts';
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <App />
+        </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
