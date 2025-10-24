37a0e96d2b0e0f2e47bdbf589969e34ff81843ca
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-24 17:35:51 +0200

    Wrapped all routes into Layout, added register route that leads to Register component

--- client/src/core/App/index.tsx
+++ client/src/core/App/index.tsx
@@ -1,12 +1,19 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom"
 import { Home } from "../../features/home";
+import { Layout } from "./Layout";
+import { Register } from "./Register";
 
 export const App = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="*" element={<Navigate to={"/home"} replace />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<Navigate to={"/home"} replace />} />
+          {/* <Route path="/auth/login" element={<Login />} /> */}
+          <Route path="/auth/register" element={<Register />} />
+          <Route path="/auth/*" element={<Navigate to="/auth/register" replace />} />
+        </Route>
       </Routes>
     </HashRouter>
   );
