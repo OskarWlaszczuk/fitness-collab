60b7281b711412e2fc53b50b7fa80ade5af0bed1
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-24 13:54:29 +0200

    Wrapped refreshAccessToken and logout controllers into asyncErrorHandler to seperate controllers logics from error handling code and make these functions cleaner

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { login, register } from "../controllers/authControllers.js";
+import { login, logout, refreshAccessToken, register } from "../controllers/authControllers.js";
+import { isRefreshTokenStoredInCoookies } from "../middlewares/isRefreshTokenStoredInCoookies.js";
+import { isRefreshTokenSecretValid } from "../middlewares/isRefreshTokenSecretValid.js";
+import { isUserExists } from "../middlewares/isUserExists.js";
+import { isRefreshTokenSessionFresh } from "../middlewares/isRefreshTokenSessionFresh.js";
 
 const authRouter = express.Router();
 
-authRouter.route("/login").
-    post(login);
-authRouter.route("/register").
-    post(register);
-
+authRouter.route("/login").post(login);
+authRouter.route("/register").post(register);
+authRouter.route("/logout").delete(isRefreshTokenStoredInCoookies, isRefreshTokenSecretValid, isUserExists, isRefreshTokenSessionFresh, logout);
+authRouter.route("/refreshAccessToken").get(isRefreshTokenStoredInCoookies, isRefreshTokenSecretValid, isUserExists, isRefreshTokenSessionFresh, refreshAccessToken);
 export default authRouter;
