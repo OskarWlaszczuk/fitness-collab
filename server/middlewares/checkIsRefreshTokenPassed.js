2f8464428f1f09cd4e109398a34fbf859b2e0c7f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-26 13:59:33 +0100

    renamed chechRefreshTokenInCookies to checkIsRefreshTokenPassed

--- server/middlewares/checkIsRefreshTokenPassed.js
+++ server/middlewares/checkIsRefreshTokenPassed.js
@@ -1,10 +1,8 @@
 import { CustomError } from "../utils/CustomError.js";
 
-export const chechRefreshTokenInCookies = (request, response, next) => {
-    console.log("checking is rt stored in coookies");
-
+export const checkIsRefreshTokenPassed = (request, response, next) => {
     const refreshToken = request.cookies?.refreshToken;
-
+    
     if (!refreshToken) {
         const error = new CustomError("refresh token is required", 400);
         return next(error);
