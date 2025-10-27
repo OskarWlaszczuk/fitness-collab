052c52d108697f8bb0c375d84a6e318096d9cb72
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-27 10:36:31 +0100

    Refactor checkIsRefreshTokenPassed middleware to use environment variable for cookie name

--- server/middlewares/checkIsRefreshTokenPassed.js
+++ server/middlewares/checkIsRefreshTokenPassed.js
@@ -1,8 +1,10 @@
+import { config } from "dotenv";
 import { CustomError } from "../utils/CustomError.js";
 
+config();
+
 export const checkIsRefreshTokenPassed = (request, response, next) => {
-    const refreshToken = request.cookies?.refreshToken;
-    
+    const refreshToken = request.cookies?.[process.env.COOKIE_NAME];
     if (!refreshToken) {
         const error = new CustomError("refresh token is required", 400);
         return next(error);
