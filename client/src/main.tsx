401f72dc009c1c1652bad2b4a93a1c7cb39cb878
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-27 09:02:59 +0100

    Wrapped App inside BrowserRouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,15 +6,18 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { GlobalStyles } from './core/GlobalStyles.ts';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './core/theme.ts';
+import { BrowserRouter } from 'react-router-dom';
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
           <App />
-        </ThemeProvider>
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
