082fb13a8e98b5f85c684405a17b3a97177aae3d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-27 10:24:14 +0100

    Added logout button. created logout fn handler, it sends delete request to remove refresh token from cookies. On success fetch clear access token from cache  and navigate user to login page. If access token is not available display links to login and register pages

--- client/src/core/App/Layout/TopPanel/index.tsx
+++ client/src/core/App/Layout/TopPanel/index.tsx
@@ -1,13 +1,41 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import { StyledTopPanel } from "./styled"
+import axios from "axios";
+import { useQueryClient } from "@tanstack/react-query";
+import { useAccessTokenQuery } from "../../../../common/hooks/useAccessTokenQuery";
 
 
 export const TopPanel = () => {
+    const { accessToken } = useAccessTokenQuery();
+
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+
+    const logout = async () => {
+        //użyć useMutation
+        try {
+            await axios.delete("http://localhost:5000/api/auth/logout", { withCredentials: true });
+            //czyszczenie cacha i przenoszenie do login wewnątrz funkcji onSuccess
+            queryClient.setQueryData(["accessToken"], null);
+            queryClient.removeQueries({ queryKey: ["accessToken"] });
+            navigate("/fitness-collab/auth/login");
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <StyledTopPanel>
-            <NavLink to="/fitness-collab/auth/login">Login</NavLink>
-            <NavLink to="/fitness-collab/auth/register">Register</NavLink>
+            {
+                !accessToken && (
+                    <>
+                        <NavLink to="/fitness-collab/auth/login">Login</NavLink>
+                        <NavLink to="/fitness-collab/auth/register">Register</NavLink>
+                    </>
+                )
+            }
+            <button onClick={logout}>logout</button>
         </StyledTopPanel>
     );
 };
