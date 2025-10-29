79ffd549638d09c0a83e294335483a0d004bdded
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-29 16:57:30 +0100

    Added interceptors for userApi client to handle its requests and responses in order to implement and manage access token refresh logic

--- client/src/core/App/index.tsx
+++ client/src/core/App/index.tsx
@@ -4,10 +4,44 @@ import { Layout } from "./Layout";
 import { Register } from "./Register";
 import { useAccessTokenQuery } from "../../common/hooks/useAccessTokenQuery";
 import { Login } from "./Login";
+import { useLayoutEffect } from "react";
+import { userApi } from "../../apiClients";
 
 export const App = () => {
-  const { accessToken } = useAccessTokenQuery();
-  console.log(accessToken);
+  const { accessToken, refetchAccessToken } = useAccessTokenQuery();
+
+  useLayoutEffect(() => {
+    userApi.interceptors.request.use((config) => {
+      if (accessToken) {
+        console.log("Adding access token to user api request", accessToken);
+        
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+
+      return config;
+    });
+
+    userApi.interceptors.response.use(
+      response => response,
+      async (error) => {
+        try {
+          const originalRequest = error.config;
+
+          if (
+            (error.status === 403 || error.status === 401) && !originalRequest._retry
+          ) {
+            originalRequest._retry = true;
+            await refetchAccessToken();
+          }
+          //po co odrzucenie obietnicy tutaj?
+          return Promise.reject(error);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
+  }, [accessToken, refetchAccessToken]);
+
   const baseUrl = "/fitness-collab";
   const baseAuthUrl = `${baseUrl}/auth`;
 
@@ -20,6 +54,9 @@ export const App = () => {
         <Route path={getRouteUrl("home")} element={<Home />} />
         <Route path="*" element={<Navigate to={getRouteUrl("home")} replace />} />
       </Route>
+      {
+        // jeżeli jest route /auth to nie pobiera access tokenu!
+      }
       <Route path={getAuthRouteUrl("login")} element={<Login />} />
       <Route path={getAuthRouteUrl("register")} element={<Register />} />
       <Route path={`${baseAuthUrl}/*`} element={<Navigate to={getAuthRouteUrl("login")} replace />} />
