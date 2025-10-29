1846ce81d7db2b328cff263237e214c07f0adef3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-29 17:04:40 +0100

    Added testing useQuery call to fetch user profile data and display them in Home component in order to test is interceptors set for userApi client in App component manages access token addiotion system  to auth headers of the userApi client requests and refreshing it authomaticly if the requests return error.

--- client/src/features/home/index.tsx
+++ client/src/features/home/index.tsx
@@ -1,5 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { userApi } from "../../apiClients";
+import { useAccessTokenQuery } from "../../common/hooks/useAccessTokenQuery";
+
 export const Home = () => {
+    const { accessToken } = useAccessTokenQuery();
+
+    const getUser = async () => {
+        try {
+            const response = await userApi.get("/profile");
+            return response.data;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const userFreshTimeMin = 60 * 60 * 1000;
+
+    const { data: userProfile } = useQuery({
+        queryKey: ["user", "profile"],
+        queryFn: getUser,
+        staleTime: userFreshTimeMin,
+        gcTime: userFreshTimeMin,
+        enabled: !!accessToken,
+        refetchOnWindowFocus: false,
+    });
+
+    console.log(userProfile);
+
     return (
-        <>Welcome</>
+        <div style={{ color: "black" }}>
+            {
+                accessToken ?
+                    <p>Welcome {userProfile?.user.nickname}</p> :
+                    <h1>Home page</h1>
+            }
+        </div>
     );
 };
