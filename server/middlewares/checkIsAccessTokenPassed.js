899f966ab1e0bf709a190f18cb94e57d198123a7
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-29 17:09:43 +0100

    fixed import path  of CustomError in checkIsAccessTokenPassed fn file

--- server/middlewares/checkIsAccessTokenPassed.js
+++ server/middlewares/checkIsAccessTokenPassed.js
@@ -1,4 +1,4 @@
-import { CustomError } from "../utils/CustomError";
+import { CustomError } from "../utils/CustomError.js";
 
 export const checkIsAccessTokenPassed = async (request, response, next) => {
     const authHeader = request.headers["authorization"];
