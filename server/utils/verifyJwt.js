72f7f703597c5baf952167ab92b6b11d890f172c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-29 18:58:11 +0100

    Expanded verifyJwt helper, made it async fn to catch and handle error thrown by promisify method and make validate middleware cleaner

--- server/utils/verifyJwt.js
+++ server/utils/verifyJwt.js
@@ -1,4 +1,12 @@
 import util from "util";
 import jwt from "jsonwebtoken";
+import { CustomError } from "./CustomError.js";
 
-export const verifyJwt = util.promisify(jwt.verify);
+export const verifyJwt = async (token, tokenSecret) => {
+    try {
+        return await util.promisify(jwt.verify)(token, tokenSecret)
+    } catch {
+        const error = new CustomError("error verifying JWT signature", 401);
+        throw error
+    }
+};
