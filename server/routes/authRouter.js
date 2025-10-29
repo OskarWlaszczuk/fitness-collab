2fbebce7e6480935f06c5d5b8bf274ac50ccb9eb
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-29 19:00:25 +0100

    Replaced validateTokenSession middleware in authRouter' s routes handlers with checkIsRefreshTokenIntergrated and checkIsTokenSessionActive midldlewares

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -3,12 +3,27 @@ import { login, logout, refreshAccessToken, register } from "../controllers/auth
 import { checkIsRefreshTokenPassed } from "../middlewares/checkIsRefreshTokenPassed.js";
 import { validateRefreshTokenSignature } from "../middlewares/validateRefreshTokenSignature.js";
 import { checkUserExists } from "../middlewares/checkUserExists.js";
-import { validateTokenSession } from "../middlewares/validateTokenSession.js";
+import { checkIsRefreshTokenIntergrated } from "../middlewares/checkIsRefreshTokenIntergrated.js";
+import { checkIsTokenSessionActive } from "../middlewares/checkIsTokenSessionActive.js";
 
 const authRouter = express.Router();
 
 authRouter.route("/login").post(login);
 authRouter.route("/register").post(register);
-authRouter.route("/logout").delete(checkIsRefreshTokenPassed, validateRefreshTokenSignature, checkUserExists, validateTokenSession, logout);
-authRouter.route("/refreshAccessToken").get(checkIsRefreshTokenPassed, validateRefreshTokenSignature, checkUserExists, validateTokenSession, refreshAccessToken);
+authRouter.route("/logout").delete(
+    checkIsRefreshTokenPassed,
+    validateRefreshTokenSignature,
+    checkUserExists,
+    checkIsTokenSessionActive,
+    checkIsRefreshTokenIntergrated,
+    logout
+);
+authRouter.route("/refreshAccessToken").get(
+    checkIsRefreshTokenPassed,
+    validateRefreshTokenSignature,
+    checkUserExists,
+    checkIsTokenSessionActive,
+    checkIsRefreshTokenIntergrated,
+    refreshAccessToken
+);
 export default authRouter;
