fc954cd76cb9649183d0c4a21c3bb300bc9bc34f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-29 19:01:11 +0100

    Wrapped validateAccessTokenSignature into asyncErrorHandler to remove try/catch block error handling responsibity from middleware itself

--- server/middlewares/validateAccessTokenSignature.js
+++ server/middlewares/validateAccessTokenSignature.js
@@ -1,16 +1,13 @@
 import { config } from "dotenv";
 import { verifyJwt } from "../utils/verifyJwt.js";
 import { CustomError } from "../utils/CustomError.js";
+import { asyncErrorHandler } from "../utils/asyncErrorHandler.js";
 config();
 
-export const validateAccessTokenSignature = async (request, response, next) => {
-    try {
-        const accessToken = request.accessToken;
-        const tokenPayload = await verifyJwt(accessToken, process.env.ACCESS_TOKEN_SECRET);
-        request.tokenPayload = tokenPayload;
-        return next();
-    } catch {
-        const error = new CustomError("error verifying access token signature", 401);
-        return next(error)
-    }
-};
+
+export const validateAccessTokenSignature = asyncErrorHandler(async (request, response, next) => {
+    const accessToken = request.accessToken;
+    const tokenPayload = await verifyJwt(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    request.tokenPayload = tokenPayload;
+    return next();
+});
