85c35232d4b154b0fb68ff203d14f48abe99b2cb
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-31 17:11:43 +0100

    removed links to login and register pages because top panel is not display if user is not auth.

--- client/src/core/App/Layout/TopPanel/index.tsx
+++ client/src/core/App/Layout/TopPanel/index.tsx
@@ -1,15 +1,15 @@
-import { NavLink, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { StyledTopPanel } from "./styled"
 import { useQueryClient } from "@tanstack/react-query";
-import { useAccessTokenQuery } from "../../../../common/hooks/useAccessTokenQuery";
 import { authApi } from "../../../../apiClients";
+import { useUserActiveModeQuery } from "../../../../common/hooks/useUserActiveModeQuery";
 
 
 export const TopPanel = () => {
-    const { accessToken } = useAccessTokenQuery();
 
     const queryClient = useQueryClient();
     const navigate = useNavigate();
+    const { activeMode } = useUserActiveModeQuery();
 
     const logout = async () => {
         //użyć useMutation
@@ -26,34 +26,18 @@ export const TopPanel = () => {
             queryClient.removeQueries({ queryKey: ["accessToken"] });
             queryClient.removeQueries({ queryKey: ["user"] });
 
-
             console.log("Cache po wylogowaniu:", accessTokenCache, profileCache);
 
-            // navigate("/fitness-collab/auth/login");
+            navigate("/login", { replace: true });
 
         } catch (error) {
             console.log(error);
         }
     };
-    const accessTokenCache = queryClient.getQueryData(['accessToken']);
-    const profileCache = queryClient.getQueryData(['user']);
-    console.log(accessTokenCache, profileCache);
 
     return (
         <StyledTopPanel>
-            {
-                !accessToken && (
-                    <>
-                        <NavLink to="/fitness-collab/auth/login">Login</NavLink>
-                        <NavLink to="/fitness-collab/auth/register">Register</NavLink>
-                    </>
-                )
-            }
-            {
-                accessToken && (
-                    <button onClick={logout}>logout</button>
-                )
-            }
+            <button onClick={logout}>logout {activeMode?.mode.name}</button>
         </StyledTopPanel>
     );
 };
