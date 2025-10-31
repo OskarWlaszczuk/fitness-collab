b4ad46b3f36cb5678c5ada27c310f765231a8126
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-31 17:23:24 +0100

    renamed wrong prop name hashedRefreshToken of user object queried from db to refresh_token_hash

--- server/middlewares/checkIsTokenSessionActive.js
+++ server/middlewares/checkIsTokenSessionActive.js
@@ -1,9 +1,9 @@
 import { CustomError } from "../utils/CustomError.js";
 
 export const checkIsTokenSessionActive = (request, response, next) => {
-    const { hashedRefreshToken } = request.user;
+    const { refresh_token_hash } = request.user;
 
-    const isSessionActive = !!hashedRefreshToken;
+    const isSessionActive = !!refresh_token_hash;
 
     if (!isSessionActive) {
         const error = new CustomError("session expired, please log in again", 401);
