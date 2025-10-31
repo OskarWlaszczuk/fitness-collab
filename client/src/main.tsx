fed309abc58901c7fdb43aca6090b36b4a0e6d81
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-31 16:43:11 +0100

    Moved BrowserRouter wrapp inside App

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,18 +6,17 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { GlobalStyles } from './core/GlobalStyles.ts';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './core/theme.ts';
-import { BrowserRouter } from 'react-router-dom';
+
 const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </QueryClientProvider>
-  </StrictMode>,
-)
+        <App />
+    </ThemeProvider>
+  </QueryClientProvider>
+  </StrictMode >
+);
