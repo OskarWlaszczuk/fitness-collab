5e8ef187dd37d2ca34d73db01749d394756a7b7d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-31 17:10:50 +0100

    call useUserActiveModeQuery hook to fetch/get from cache activeMode state. Removed navItemsConfig and replaced it with sharedNavConfig, traineeNavConfig and trainerNavConfig in order to select nav config based on user mode

--- client/src/core/App/Layout/NavPanel/index.tsx
+++ client/src/core/App/Layout/NavPanel/index.tsx
@@ -1,18 +1,53 @@
+import { useUserActiveModeQuery } from "../../../../common/hooks/useUserActiveModeQuery";
 import { NavItem, NavItemIcon, NavItemLabel, StyledNavPanel } from "./styled"
 
 export const NavPanel = () => {
-    const navItemsConfig = [
+    const { activeMode, status } = useUserActiveModeQuery();
+
+
+    const sharedNavConfig = [
         {
             icon: "",
             label: "Home",
-            routePath: "home/"
+            routePath: "/home"
+        },
+        {
+            icon: "",
+            label: "Chats",
+            routePath: "/chats"
+        },
+        {
+            icon: "",
+            label: "Profile",
+            routePath: "/profile"
         },
     ];
+
+    const traineeNavConfig = [
+        ...sharedNavConfig,
+        {
+            icon: "",
+            label: "workouts",
+            routePath: "/workouts"
+        },
+    ]
+
+    const trainerNavConfig = [
+        ...sharedNavConfig,
+        {
+            icon: "",
+            label: "workout creator",
+            routePath: "/workout-creator"
+        },
+    ]
+
+    const navConfig = activeMode?.mode.id === 1 ? traineeNavConfig : trainerNavConfig;
+
 
     return (
         <StyledNavPanel>
             {
-                navItemsConfig.map(({ icon, label, routePath }) => (
+                navConfig.map(({ icon, label, routePath }) => (
                     <NavItem to={routePath} key={label}>
                         <NavItemIcon>{icon}</NavItemIcon>
                         <NavItemLabel>{label}</NavItemLabel>
