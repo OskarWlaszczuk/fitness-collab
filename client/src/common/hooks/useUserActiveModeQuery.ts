77494ecfe745fea80e2bb09d58b77a289d1937cc
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-10-31 16:55:34 +0100

    created useUserActiveModeQuery hook that uses userApi client to fetch user current mode

---
+++ client/src/common/hooks/useUserActiveModeQuery.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { userApi } from "../../apiClients";
+import type { UserMode } from "../../features/Login";
+import { useAccessTokenQuery } from "./useAccessTokenQuery";
+
+interface UserActiveModeResponse {
+    mode: UserMode;
+}
+
+export const useUserActiveModeQuery = () => {
+    //wywoływać useAccessTokenQuery tu czy poza hookiem i przekazywać at w param?
+    const { accessToken } = useAccessTokenQuery();
+
+    const refreshTimeMin = 15 * 60 * 1000;
+    //jak obsługiwać błędu queryFn?
+    const fetchUserActiveMode = async (): Promise<UserActiveModeResponse> => {
+        const response = await userApi.get("/user/activeMode");
+        return response.data;
+    };
+
+    const { data: activeMode, status, isPaused, error } = useQuery<UserActiveModeResponse>({
+        queryKey: ["activeMode"],
+        queryFn: fetchUserActiveMode,
+        staleTime: refreshTimeMin,
+        gcTime: refreshTimeMin,
+        refetchInterval: refreshTimeMin,
+        retry: 1,
+        enabled: !!accessToken,
+    });
+
+    return {
+        status,
+        activeMode,
+        isPaused,
+        error,
+    };
+};
