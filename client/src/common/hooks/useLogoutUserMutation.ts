9d173b17b4464bdbcbdaf9ea61fa4db18f75fcac
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-11-01 14:00:53 +0100

    refactored useLogoutUserMutation: deleted onLoginError, asssigned onLoginSuccess fn body directly to onSuccess field in useMutation call. Set access token to null in cache on success request to end user session

--- client/src/common/hooks/useLogoutUserMutation.ts
+++ client/src/common/hooks/useLogoutUserMutation.ts
@@ -1,31 +1,23 @@
-import { useMutation } from "@tanstack/react-query";
-import { useNavigate, type NavigateFunction } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { authApi } from "../../apiClients";
 
 const logoutUser = async () => {
-    try {
-        await authApi.delete("/logout");
-    } catch (error) {
-        console.error("Login request error:", error);
-        throw error;
-    }
-};
-
-const onLogoutSuccess = (navigate: NavigateFunction) => {
-    navigate("/home", { replace: true });
-};
-
-const onLoginError = (error: unknown) => {
-    console.error("Login error:", error);
+    await authApi.delete("/logout");
 };
 
 export const useLogoutUserMutation = () => {
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
 
     const { mutate: logout, isPending, isSuccess, error, isError } = useMutation({
         mutationFn: logoutUser,
-        onSuccess: () => onLogoutSuccess(navigate),
-        onError: (error) => onLoginError(error)
+        onSuccess: () => {
+            //czy powinienem wszystkie dane użytkownika usunąć z cache?
+            queryClient.setQueryData(["accessToken"], null);
+
+            navigate("/login", { replace: true });
+        },
     });
 
     return {
