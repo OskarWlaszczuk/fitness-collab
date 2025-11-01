985c9bb8ade59c8f58f5fdcf7f7678d775902a5b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-11-01 08:58:04 +0100

    Created public subfolder inside features folder for nonauthorized pages like login, register and missing

--- client/src/core/App/index.tsx
+++ client/src/core/App/index.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import {  Navigate, Route, Routes } from "react-router-dom"
 import { Home } from "../../features/Home";
 import { Layout } from "./Layout";
-import { Register } from "../../features/Register";
-import { Login } from "../../features/Login";
+import { Register } from "../../features/public/Register";
+import { Login } from "../../features/public/Login";
 import { AuthRoute } from "./routes/AuthRoute";
 import { ProtectedRoute } from "./routes/ProtectedRoute";
 import { Chats } from "../../features/Chats";
@@ -10,82 +10,38 @@ import { ModeProtectedRoute } from "./routes/ModeProtectedRoute";
 import { Workouts } from "../../features/Workouts";
 import { WorkoutCreator } from "../../features/WorkoutCreator";
 import { Unauthorized } from "../../features/Unauthorized";
-import { Missing } from "../../features/Missing";
+import { Missing } from "../../features/public/Missing";
 import { useUserApiInterceptors } from "./useUserApiInterceptors";
 
 export const App = () => {
   useUserApiInterceptors();
 
   return (
-    <BrowserRouter basename="/fitness-collab" >
-      <Routes >
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route element={<AuthRoute />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
+    <Routes >
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route element={<AuthRoute />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
 
-        <Route element={<Layout />}>
-          <Route element={<ProtectedRoute />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/chats" element={<Chats />} />
-            <Route path="/profile" element={<>Profile</>} />
+      <Route element={<Layout />}>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/profile" element={<>Profile</>} />
 
-            <Route element={<ModeProtectedRoute allowedModes={[1]} />}>
-              <Route path="/workouts" element={<Workouts />} />
-            </Route>
-
-            <Route element={<ModeProtectedRoute allowedModes={[2]} />}>
-              <Route path="/workout-creator" element={<WorkoutCreator />} />
-            </Route>
-
-            <Route path="/unauthorized" element={<Unauthorized />} />
+          <Route element={<ModeProtectedRoute allowedModes={[1]} />}>
+            <Route path="/workouts" element={<Workouts />} />
           </Route>
+
+          <Route element={<ModeProtectedRoute allowedModes={[2]} />}>
+            <Route path="/workout-creator" element={<WorkoutCreator />} />
+          </Route>
+
+          <Route path="/unauthorized" element={<Unauthorized />} />
         </Route>
-        <Route path="*" element={<Missing />} />
-      </Routes>
-    </BrowserRouter>
+      </Route>
+      <Route path="*" element={<Missing />} />
+    </Routes>
   );
 };
-
-
-
-
-
-
-
-// return (
-//   <Routes>
-//     <Route path="/auth">
-//       <Route path="/auth/login" element={<Login />} />
-//       <Route path="/auth/register" element={<Register />} />
-//     </Route>
-
-//     <Route path="/" element={<Layout />} >
-//       {
-//         //ścieżki login i register nie dostępne jeżeli !!accessToken
-//       }
-//       {/* <Route path="/login" element={<Login />} />
-//       <Route path="/register" element={<Register />} /> */}
-//       <Route path="/unathorized" element={<Unathorized />} />
-
-//       <Route element={<RequireAuth allowedModes={[1, 2]} />}>
-//         <Route path="/home" element={<Home />} />
-//         <Route path="/chats" element={<Chats />} />
-//         <Route path="/profile" element={<>Profile</>} />
-//       </Route>
-
-//       <Route element={<RequireAuth allowedModes={[1]} />}>
-//         <Route path="/workouts" element={<>workouts</>} />
-//         <Route path="/trainers" element={<>trainers</>} />
-//         <Route path="/excersise" element={<>excersise</>} />
-//       </Route>
-
-//       <Route element={<RequireAuth allowedModes={[2]} />}>
-//         <Route path="/workout-creator" element={<>workout creator</>} />
-//       </Route>
-
-//       <Route path="*" element={<Missing />} />
-//     </Route>
-//   </Routes>
-// );
