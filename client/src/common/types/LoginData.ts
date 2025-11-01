d345489279139ca13c17388b633b4818b868c4a6
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-11-01 09:27:23 +0100

    refactor(LoginData): remove undefined from modeId type

--- client/src/common/types/LoginData.ts
+++ client/src/common/types/LoginData.ts
@@ -3,5 +3,5 @@ import type { UserMode } from "./UserMode";
 export interface LoginData {
     email: string;
     password: string;
-    modeId: UserMode["id"] | undefined;
+    modeId: UserMode["id"];
 }
