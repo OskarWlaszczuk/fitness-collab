6a0e6409cf29f0e38b6b8082707a2063ab9eeaff
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-11-01 13:55:32 +0100

    refactor(useAccessTokenQuery): update refreshAccessToken to return AccessToken type

--- client/src/common/hooks/useAccessTokenQuery.ts
+++ client/src/common/hooks/useAccessTokenQuery.ts
@@ -1,15 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import { authApi } from "../../apiClients";
+import type { AccessToken } from "../types/AccessToken";
+
+export interface AccessTokenResponse {
+    accessToken: AccessToken;
+}
 
 export const useAccessTokenQuery = () => {
     const refreshTimeMin = 15 * 60 * 1000;
     //jak obsługiwać błędu queryFn?
-    const refreshAccessToken = async (): Promise<string> => {
-        const response = await authApi.get("/refreshAccessToken");
+    const refreshAccessToken = async (): Promise<AccessToken> => {
+        const response = await authApi.get<AccessTokenResponse>("/refreshAccessToken");
         return response.data.accessToken;
     };
 
-    const { data: accessToken, status, isPaused, error, refetch: refetchAccessToken } = useQuery<string>({
+    const { data: accessToken, status, isPaused, error, refetch: refetchAccessToken } = useQuery({
         queryKey: ["accessToken"],
         queryFn: refreshAccessToken,
         staleTime: refreshTimeMin,
