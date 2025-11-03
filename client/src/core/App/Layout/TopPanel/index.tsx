410ee5f6272d84a67933ea072187976ec6363881
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-11-03 21:12:10 +0100

    fixed  logout onCLick handler in TopPanel

--- client/src/core/App/Layout/TopPanel/index.tsx
+++ client/src/core/App/Layout/TopPanel/index.tsx
@@ -2,16 +2,13 @@ import { StyledTopPanel } from "./styled"
 import { useUserActiveModeQuery } from "../../../../common/hooks/useUserActiveModeQuery";
 import { useLogoutUserMutation } from "../../../../common/hooks/useLogoutUserMutation";
 
-
-
-
 export const TopPanel = () => {
     const { logout } = useLogoutUserMutation();
     const { activeMode } = useUserActiveModeQuery();
 
     return (
         <StyledTopPanel>
-            <button onClick={logout}>logout {activeMode?.mode.name}</button>
+            <button onClick={()=> logout()}>logout {activeMode?.name}</button>
         </StyledTopPanel>
     );
 };
