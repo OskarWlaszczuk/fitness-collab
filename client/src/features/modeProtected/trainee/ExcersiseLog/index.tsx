ec538d9405a8182f17d2667c55410000b2a6e069
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-11-08 12:54:02 +0100

    created initial and testing version of ExcersiseLog component. It will display trainee's  list of excersise ever used

---
+++ client/src/features/modeProtected/trainee/ExcersiseLog/index.tsx
@@ -0,0 +1,5 @@
+export const ExcersiseLog = () => {
+    return (
+        <>Trainee excersise log</>
+    );
+};
---
+++ client/src/features/modeProtected/trainee/ExcersiseLogEntries/index.tsx
@@ -0,0 +1,7 @@
+export const ExcersiseLogEntries = () => {
+    return (
+        <>
+            Excersise Log Entries
+        </>
+    );
+};
