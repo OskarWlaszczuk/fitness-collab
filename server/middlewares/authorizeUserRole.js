0bdb8d450259c113f031d91c4ba6f6786c75cad1
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-11-12 21:07:03 +0100

    formatted code in authorizeUserRole fn

--- server/middlewares/authorizeUserRole.js
+++ server/middlewares/authorizeUserRole.js
@@ -4,11 +4,11 @@ export const authorizeUserRole = (allowedRoleIds) => {
     return async (request, response, next) => {
         const tokenPayload = request.tokenPayload;
         console.log(`Allowed role: ${allowedRoleIds}, token role: ${tokenPayload.roleId}`);
-
-        if (allowedRoleIds.includes(tokenPayload.roleId)) {
+        if (!allowedRoleIds.includes(tokenPayload.roleId)) {
             const error = new CustomError("user is not register on this role", 403);
             return next(error);
         }
+
         return next();
     };
 };
