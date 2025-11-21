915704d539e5e0a7982e755aeaf96573a1f9f304
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-11-21 12:23:31 +0100

    removed getTraineeWorkoutPlan and getTraineeWorkoutPlanDay controller functions from traineeController file and moved them to seperated files

--- server/routes/traineeRouter.js
+++ server/routes/traineeRouter.js
@@ -2,18 +2,15 @@ import express from "express";
 import {
     getTraineeProfile,
     getTraineeWorkoutPlans,
-    getTraineeWorkoutPlan,
-    getTraineeWorkoutPlanDay,
     getTraineeExcersiseLastEntrySets,
     getExcersiseInstructions,
 } from "../controllers/traineeControllers.js";
 
 const traineeRouter = express.Router();
 
-traineeRouter.get("/workout-plans/:id", getTraineeWorkoutPlan);
 traineeRouter.get("/workout-plans", getTraineeWorkoutPlans);
 traineeRouter.get("/profile", getTraineeProfile);
-traineeRouter.get("/workout-plan-day/:id", getTraineeWorkoutPlanDay);
+
 traineeRouter.get("/excersise/:id/entry", getTraineeExcersiseLastEntrySets);
 traineeRouter.get("/excersise/:id/instructions", getExcersiseInstructions);
 
