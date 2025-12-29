dbf61ba9025bdec051d398df3f1073c44c942533
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-12-29 13:54:43 +0100

    add ExerciseSetRow component to render exercise set inputs dynamically based on passed inputs configuration.  For each input fileld binds its value from set object using name key

---
+++ client/src/common/components/WorkoutExerciseDetails/ExerciseSets/ExerciseSetRow/index.tsx
@@ -0,0 +1,25 @@
+export const ExerciseSetRow = ({ set, onFieldChange, setFieldsConfig }) => {
+    //system sprawdzania wiersza serii po zmianie jednego z jego pól
+    return (
+        <div>
+            {
+                setFieldsConfig.map(setConfig => (
+                    <>
+                        <input
+                            name={setConfig.name}
+                            value={set[setConfig.name]}
+                            type={setConfig.type}
+                            onChange={(event) => {
+                                //jeden wspólny handler dla wszystkich pól serii
+                                onFieldChange(event);
+                            }}
+                            min={0}
+                        >
+                            <label>{setConfig.label}</label>
+                        </input>
+                    </>
+                ))
+            }
+        </div>
+    );
+};
