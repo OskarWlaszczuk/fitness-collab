17ae07631ffaafa0c741366117f4e75c2f051d35
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-12-30 15:46:39 +0100

    Refactor IdbSetRows component to implement update and delete functionality for exercise sets (only in UI state for now), and enhance onSetChange handler for tracking changes.

--- client/src/common/components/WorkoutExerciseDetails/ExerciseSets/IdbSetRows/index.tsx
+++ client/src/common/components/WorkoutExerciseDetails/ExerciseSets/IdbSetRows/index.tsx
@@ -1,39 +1,8 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { useExerciseIdbSetsQuery, type IdbSet } from "../../../../hooks/useExerciseIdbSetsQuery";
 import { ExerciseSetRow } from "../../../ExerciseSetRow";
-import { useAddNewIdbSetMutation } from "../../../../hooks/useSaveIDBSet";
-import { useUpdateIdbSetMutation } from "../../../../hooks/useUpdateIDBSet";
-
-// const useSaveDraftSet = ({ draftSet, setDraftSet, exerciseId }) => {
-//     useEffect(() => {
-//         //sprawdzenie, czy wszystkie pola draft set zostały uzupełnione - zapis nowej serii IDB
-//         if (draftSet && Object.values(draftSet).every(value => value)) {
-//             console.log("Saving new completed set", draftSet);
-//             addNewIdbSetMutation.mutate({ set: draftSet, exerciseId });
-
-//             if (addNewIdbSetMutation.isSuccess) {
-//                 console.log('draft zapisany');
-//             }
-//             //wywołać saveIdbSet, zapisać draft set w idb, dodać od razu do query idb sets, aby zaktualizować widok i ustawić stan draftSet, jako undefined
-//         }
-//     }, [draftSet, exerciseId, addNewIdbSetMutation, setDraftSet]);
-// }
-
-// export const useUpdateIdbSet = ({ exerciseId, idbSets }) => {
-//     useEffect(() => {
-//         //sprawdzenie, czy któryś z zapisanych serii IDB został edytowany
-//         if (idbSets && idbSets.length) {
-//             idbSets.forEach(set => {
-//                 //sprawdzenie, czy wszystkie pola edytowanego obiektu są uzupełnione
-//                 if (Object.values(set).every(value => value) && set.hasChanged) {
-//                     console.log("Update serii", set);
-
-//                     // updateIdbSetMutation.mutate(set);
-//                 }
-//             })
-//         }
-//     }, [idbSets]);
-// };
+// import { useAddNewIdbSetMutation } from "../../../../hooks/useSaveIDBSet";
+// import { useUpdateIdbSetMutation } from "../../../../hooks/useUpdateIDBSet";
 
 export const IdbSetRows = ({ exerciseId, setFieldsConfig }) => {
     const queryClient = useQueryClient();
@@ -41,40 +10,65 @@ export const IdbSetRows = ({ exerciseId, setFieldsConfig }) => {
     // const addNewIdbSetMutation = useAddNewIdbSetMutation();
     // const updateIdbSetMutation = useUpdateIdbSetMutation();
 
+    const updateIdbSet = (set) => {
+        queryClient.setQueryData(
+            ["idb", "exerciseSets", exerciseId],
+            (sets: IdbSet[]) => (
+                [
+                    ...sets.filter(({ id }) => id !== set.id),
+                    set
+                ]
+            )
+        );
+    };
+
+    const deleteIdbSet = (set) => {
+        queryClient.setQueryData(
+            ["idb", "exerciseSets", exerciseId],
+            (sets: IdbSet[]) => (
+                [
+                    ...sets.filter(({ id }) => id !== set.id),
+                ]
+            )
+        );
+    };
+
+    const onIdbSetChange = (updatedSet) => {
+        console.log(`set ${updatedSet.id} zmienił`);
+        const isSetCompleted = Object.values(updatedSet).every(field => !!field);
+
+        if (isSetCompleted) {
+            console.log(`set completed`);
+        }
+    };
+
+    if (!exerciseIdbSetsQuery.data) {
+        return <>loading...</>
+    }
+
     return (
         <>
             {
                 exerciseIdbSetsQuery.data
-                    ?.map(set => (
+                    .sort((a, b) => a.id - b.id)
+                    .map(set => (
                         <>
                             <ExerciseSetRow
+                                onSetChange={onIdbSetChange}
                                 set={set}
                                 setFieldsConfig={setFieldsConfig}
                                 onFieldChange={({ target }) => {
-                                    const name = target.name;
-                                    const value = target.value;
-                                    //zmieniając sam stan po edycji pola, nie zmieniam stale danych w indexedDB!
-                                    queryClient.setQueryData(
-                                        ["idb", "exerciseSets", exerciseId],
-                                        (sets: IdbSet[]) => (
-                                            sets && sets.length ?
-                                                [
-                                                    ...sets.filter(({ id }) => id !== set.id),
-                                                    {
-                                                        ...set,
-                                                        [name]: value,
-                                                        hasChanged: true,
-                                                    }
-                                                ] :
-                                                sets
-                                        )
-                                    );
+                                    const changedField = target.name;
+                                    const newFieldValue = target.value;
+                                    //update stanu widoku
+                                    const updatedSet = { ...set, [changedField]: newFieldValue, hasChanged: true };
+                                    updateIdbSet(updatedSet);
                                 }}
                             />
                             <button
                                 onClick={() => {
-                                    //wywołanie mutacji, która usunie serię z idb
-                                    //muszę
+                                    deleteIdbSet(set);
+                                    //usunięcie serii w IDB
                                 }}
                             >
                                 delete
