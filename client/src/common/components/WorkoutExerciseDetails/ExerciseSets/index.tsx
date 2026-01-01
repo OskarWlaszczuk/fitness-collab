3bbf6bf4bd47631cb98b0d0db7b50a861203ebcc
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2026-01-01 19:48:35 +0100

    Refactor ExerciseSets component to integrate exercise set mutations and enhance state management for draft sets, including delete functionality and improved onSetChange handling.

--- client/src/common/components/WorkoutExerciseDetails/ExerciseSets/index.tsx
+++ client/src/common/components/WorkoutExerciseDetails/ExerciseSets/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import type { Set } from "../../../types/Set";
 import { ExerciseSetRow } from "../../ExerciseSetRow";
-import { IdbSetRows } from "./IdbSetRows";
+import { useExerciseIdbSetsQuery } from "../../../hooks/useExerciseIdbSetsQuery";
+import { useSaveIdbSetMutation } from "../../../hooks/useSaveIdbSet";
+import { useDeleteIdbSetMutation } from "../../../hooks/useDeleteIdbSetMutation";
+import { useUpdateIdbSetMutation } from "../../../hooks/useUpdateIdbSet.ts";
 import { useQueryClient } from "@tanstack/react-query";
-import type { IdbSet } from "../../../hooks/useExerciseIdbSetsQuery";
 
 interface ExerciseSetsProps {
     exerciseId: number;
@@ -14,53 +16,58 @@ const defaultDraftSet: Set = {
     reps: undefined,
     rir: undefined,
 };
-export const ExerciseSets = ({ exerciseId }: ExerciseSetsProps) => {
 
-    //na którym etapie i jak dobierać konfigurację wiersza serii?
-    //jedna konfiguracja przynależy do wszystkich wierszy danego ćwiczenia
-    //każde ćwiczenie ma przypisany typ konfiguracji serii (pomiaru progresu) zapisane w db
-    const setFieldsConfig = [
-        {
-            name: "reps",
-            type: "number",
-            label: "x",
-        },
-        {
-            name: "weightKg",
-            type: "number",
-            label: "kg",
-        },
-        {
-            name: "rir",
-            type: "number",
-            label: "RiR",
-        },
-    ];
+const setFieldsConfig = [
+    {
+        name: "reps",
+        type: "number",
+        label: "x",
+    },
+    {
+        name: "weightKg",
+        type: "number",
+        label: "kg",
+    },
+    {
+        name: "rir",
+        type: "number",
+        label: "RiR",
+    },
+];
+
+export const ExerciseSets = ({ exerciseId }: ExerciseSetsProps) => {
+    const [draftSet, setDraftSet] = useState<Set | undefined>(defaultDraftSet);
     const queryClient = useQueryClient();
 
-    const [draftSet, setDraftSet] = useState<Set | undefined>(defaultDraftSet);
+    const exerciseIdbSetsQuery = useExerciseIdbSetsQuery(exerciseId);
+    const addNewIdbSetMutation = useSaveIdbSetMutation();
+    const updateIdbSetMutation = useUpdateIdbSetMutation(exerciseId);
+    const deleteIdbSetMutation = useDeleteIdbSetMutation(exerciseId);
 
-    const addNewIdbSet = (newSet) => {
-        queryClient.setQueryData(
-            ["idb", "exerciseSets", exerciseId],
-            (sets: IdbSet[]) => (
-                [
-                    ...sets,
-                    newSet
-                ]
-            )
-        );
+    const onIdbSetChange = (updatedSet) => {
+        const isSetCompleted = Object.values(updatedSet).every(field => !!field);
+        const { hasChanged, ...set } = updatedSet;
+
+        if (isSetCompleted) {
+            updateIdbSetMutation.mutate(set)
+        }
     };
 
     const onDraftSetChange = (updatedSet) => {
-        console.log(`set ${updatedSet.id} zmienił`);
         const isSetCompleted = Object.values(updatedSet).every(field => !!field);
+        const { hasChanged, ...set } = updatedSet;
 
         if (isSetCompleted) {
-            console.log(`draft set completed`);
-            addNewIdbSet(draftSet);
+            addNewIdbSetMutation.mutate({ set, exerciseId })
+            setDraftSet(undefined);
         }
     };
+
+    const idbSets = exerciseIdbSetsQuery.data;
+
+    if (!idbSets) {
+        return <>loading...</>
+    }
 
     return (
         <>
@@ -70,7 +77,39 @@ export const ExerciseSets = ({ exerciseId }: ExerciseSetsProps) => {
             >
                 Add set
             </button><br />
-            <IdbSetRows setFieldsConfig={setFieldsConfig} exerciseId={exerciseId} />
+            {
+                idbSets
+                    .sort((a, b) => a.id - b.id)
+                    .map(set => (
+                        <>
+                            <ExerciseSetRow
+                                onSetChange={onIdbSetChange}
+                                set={set}
+                                setFieldsConfig={setFieldsConfig}
+                                onFieldChange={({ target }) => {
+                                    const changedField = target.name;
+                                    const newFieldValue = target.value;
+                                    //update stanu widoku
+                                    const updatedSet = { ...set, [changedField]: newFieldValue, hasChanged: true };
+                                    queryClient.setQueryData(
+                                        ["idb", "exerciseSets", exerciseId],
+                                        (sets) => [
+                                            ...sets.filter(({ id }) => id !== updatedSet.id) || [],
+                                            updatedSet
+                                        ]
+                                    );
+                                }}
+                            />
+                            <button
+                                onClick={() => {
+                                    deleteIdbSetMutation.mutate(set.id)
+                                }}
+                            >
+                                delete
+                            </button>
+                        </>
+                    ))
+            }
             {
                 draftSet && (
                     <>
@@ -82,7 +121,7 @@ export const ExerciseSets = ({ exerciseId }: ExerciseSetsProps) => {
                                 const fieldName = target.name;
                                 const fieldValue = target.value;
 
-                                setDraftSet(set => ({ ...set!, [fieldName]: fieldValue }));
+                                setDraftSet(set => ({ ...set!, [fieldName]: fieldValue, hasChanged: true }));
                             }}
                         />
                         <button
