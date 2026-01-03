420fc3e2db0b85c7c6bc5ea8501016fcfb50cdd6
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2026-01-03 13:00:59 +0100

    Wrapped sets into ordered list to display set number for each row. Passed onSuccess handlers to mutation calls to update view after done iDB operation. Added useEffect call to track isSuccess state of add new idb set mutation is true - if yes then clear draft set state

--- client/src/common/components/WorkoutExerciseDetails/ExerciseSets/index.tsx
+++ client/src/common/components/WorkoutExerciseDetails/ExerciseSets/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Set } from "../../../types/Set";
 import { ExerciseSetRow } from "../../ExerciseSetRow";
 import { useExerciseIdbSetsQuery } from "../../../hooks/useExerciseIdbSetsQuery";
@@ -10,12 +10,6 @@ import { useQueryClient } from "@tanstack/react-query";
 interface ExerciseSetsProps {
     exerciseId: number;
 }
-
-const defaultDraftSet: Set = {
-    weightKg: undefined,
-    reps: undefined,
-    rir: undefined,
-};
 
 const setFieldsConfig = [
     {
@@ -36,100 +30,160 @@ const setFieldsConfig = [
 ];
 
 export const ExerciseSets = ({ exerciseId }: ExerciseSetsProps) => {
-    const [draftSet, setDraftSet] = useState<Set | undefined>(defaultDraftSet);
     const queryClient = useQueryClient();
 
     const exerciseIdbSetsQuery = useExerciseIdbSetsQuery(exerciseId);
     const addNewIdbSetMutation = useSaveIdbSetMutation();
-    const updateIdbSetMutation = useUpdateIdbSetMutation(exerciseId);
+    const updateIdbSetMutation = useUpdateIdbSetMutation();
     const deleteIdbSetMutation = useDeleteIdbSetMutation(exerciseId);
+    const [draftSet, setDraftSet] = useState({
+        weightKg: undefined,
+        reps: undefined,
+        rir: undefined,
+    });
 
     const onIdbSetComplete = (completedSet) => {
-        updateIdbSetMutation.mutate(completedSet);
+        updateIdbSetMutation.mutate(
+            completedSet,
+            {
+                onSuccess: (updatedSet) => {
+                    queryClient.setQueryData(
+                        ["idb", "exerciseSets", exerciseId],
+                        (sets) => [
+                            ...sets.filter(({ id }) => id !== updatedSet.id) || [],
+                            updatedSet
+                        ]
+                    );
+                },
+            }
+        );
     };
 
     const onDraftSetComplete = (completedSet) => {
-        console.log("Saving new idb set");
-
-        addNewIdbSetMutation.mutate({ set: completedSet, exerciseId });
-
-        // setDraftSet(undefined);
+        addNewIdbSetMutation.mutate(
+            { set: completedSet, exerciseId },
+            {
+                onSuccess: ({ set, exerciseId }) => {
+                    queryClient.setQueryData(
+                        ["idb", "exerciseSets", exerciseId],
+                        (sets) => ([
+                            ...sets,
+                            set,
+                        ])
+                    );
+                },
+            }
+        );
     };
 
     const idbSets = exerciseIdbSetsQuery.data;
 
-    if (!idbSets) {
-        return <>loading...</>
-    }
+    useEffect(() => {
+        if (addNewIdbSetMutation.isSuccess) {
+            setDraftSet(undefined);
+        }
+    }, [addNewIdbSetMutation.isSuccess]);
+
+
 
     return (
         <>
             <button
                 disabled={!!draftSet}
-                onClick={() => { setDraftSet(defaultDraftSet) }}
+                onClick={() => {
+                    setDraftSet({
+                        weightKg: undefined,
+                        reps: undefined,
+                        rir: undefined,
+                    })
+                }}
             >
                 Add set
             </button><br />
-            {
-                idbSets
-                    .sort((a, b) => a.id - b.id)
-                    .map(set => (
-                        <>
+            <ol type="I">
+                {
+                    idbSets
+                        ?.sort((a, b) => a.id - b.id)
+                        .map((set) => (
+                            <li>
+                                <ExerciseSetRow
+                                    onSetComplete={onIdbSetComplete}
+                                    set={set}
+                                    setFieldsConfig={setFieldsConfig}
+                                    onFieldChange={({ target }) => {
+                                        const changedField = target.name;
+                                        const newFieldValue = target.value;
+                                        //update stanu widoku
+                                        const updatedSet = { ...set, [changedField]: newFieldValue };
+                                        queryClient.setQueryData(
+                                            ["idb", "exerciseSets", exerciseId],
+                                            (sets) => [
+                                                ...sets.filter(({ id }) => id !== updatedSet.id) || [],
+                                                updatedSet
+                                            ]
+                                        );
+                                    }}
+                                    isSetDisabled={updateIdbSetMutation.isPending}
+                                />
+                                <button
+                                    onClick={() => {
+                                        deleteIdbSetMutation.mutate(
+                                            set.id,
+                                            {
+                                                onSuccess: (setId) => {
+                                                    queryClient.setQueryData(
+                                                        ["idb", "exerciseSets", exerciseId],
+                                                        (sets) => (
+                                                            [...sets || []].filter(({ id }) => id !== setId)
+                                                        )
+                                                    );
+                                                }
+                                            }
+                                        );
+                                    }}
+                                >
+                                    delete
+                                </button>
+                                {
+                                    (set.id === addNewIdbSetMutation.data?.set.id) && (
+                                        <p>Set saved</p>
+                                    )
+                                }
+                                {
+                                    (set.id === updateIdbSetMutation.data?.id) && (
+                                        <p>Set updated</p>
+                                    )
+                                }
+                            </li>
+                        ))
+                }
+                {
+                    draftSet && (
+                        <li>
                             <ExerciseSetRow
-                                onSetComplete={onIdbSetComplete}
-                                set={set}
+                                onSetComplete={onDraftSetComplete}
+                                set={draftSet}
                                 setFieldsConfig={setFieldsConfig}
                                 onFieldChange={({ target }) => {
-                                    const changedField = target.name;
-                                    const newFieldValue = target.value;
-                                    //update stanu widoku
-                                    const updatedSet = { ...set, [changedField]: newFieldValue };
-                                    queryClient.setQueryData(
-                                        ["idb", "exerciseSets", exerciseId],
-                                        (sets) => [
-                                            ...sets.filter(({ id }) => id !== updatedSet.id) || [],
-                                            updatedSet
-                                        ]
-                                    );
+                                    const fieldName = target.name;
+                                    const fieldValue = target.value;
+
+
+                                    setDraftSet(set => ({ ...set!, [fieldName]: fieldValue }));
                                 }}
-                                isSetDisabled={updateIdbSetMutation.isPending}
+                                isSetDisabled={addNewIdbSetMutation.isPending}
                             />
                             <button
                                 onClick={() => {
-                                    deleteIdbSetMutation.mutate(set.id);
+                                    setDraftSet(undefined);
                                 }}
                             >
-                                delete
+                                Delete
                             </button>
-                        </>
-                    ))
-            }
-            {
-                draftSet && (
-                    <>
-                        <ExerciseSetRow
-                            onSetComplete={onDraftSetComplete}
-                            set={draftSet}
-                            setFieldsConfig={setFieldsConfig}
-                            onFieldChange={({ target }) => {
-                                const fieldName = target.name;
-                                const fieldValue = target.value;
-                                console.log("updating the field");
-
-                                setDraftSet(set => ({ ...set!, [fieldName]: fieldValue }));
-                            }}
-                            isSetDisabled={addNewIdbSetMutation.isPending}
-                        />
-                        <button
-                            onClick={() => {
-                                setDraftSet(undefined);
-                            }}
-                        >
-                            Delete
-                        </button>
-                    </>
-                )
-            }
+                        </li>
+                    )
+                }
+            </ol>
         </>
     );
 };
