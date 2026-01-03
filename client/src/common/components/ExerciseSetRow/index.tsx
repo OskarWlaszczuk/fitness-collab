8b1c109f6384945a44b8840a545ccaa2a41e5442
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2026-01-03 15:21:46 +0100

    Replaced useEffect hook with useLayoutEffect. Removed hasChanged key from set prop and replace it with useRef hasSetChanged constant to keep its boolean between re-renders. I can also change  useRef object prop inside effect callback without worrrieng about triggering the infinite loop.

--- client/src/common/components/ExerciseSetRow/index.tsx
+++ client/src/common/components/ExerciseSetRow/index.tsx
@@ -1,12 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
+import { useDebounce } from "../../hooks/useDebounce";
 
-export const ExerciseSetRow = ({ set, onFieldChange, setFieldsConfig, onSetChange }) => {
+export const ExerciseSetRow = ({
+    set,
+    onFieldChange,
+    setFieldsConfig,
+    onSetComplete,
+    isSetDisabled,
+}) => {
+    const [timeoutId, debounce] = useDebounce();
 
-    useEffect(() => {
-        if (set.hasChanged) {
-            onSetChange(set);
-        }
-    }, [set, onSetChange]);
+    const hasSetChanged = useRef(false);
+
+    useLayoutEffect(
+        () => {
+            if (hasSetChanged.current) {
+                debounce(() => {
+                    const isSetCompleted = Object.values(set).every(field => !!field);
+
+                    if (isSetCompleted) {
+                        onSetComplete(set);
+                        hasSetChanged.current = false;
+                    }
+                    //nie mogę mutować właściwości stanu/props bezpośrednio 
+                    // set.hasChanged = false
+                }, 1000);
+            }
+
+            return () => {
+                const id = timeoutId.current;
+                clearTimeout(id);
+            };
+        },
+        [set, onSetComplete, timeoutId, debounce, hasSetChanged]
+    );
 
     return (
         <div>
@@ -14,17 +41,17 @@ export const ExerciseSetRow = ({ set, onFieldChange, setFieldsConfig, onSetChang
                 setFieldsConfig.map(setConfig => (
                     <>
                         <input
+                            disabled={isSetDisabled}
                             name={setConfig.name}
                             value={set[setConfig.name]}
                             type={setConfig.type}
                             onChange={(event) => {
-                                //jeden wspólny handler dla wszystkich pól serii
                                 onFieldChange(event);
+                                hasSetChanged.current = true;
                             }}
                             min={0}
                         />
                         <label>{setConfig.label}</label>
-
                     </>
                 ))
             }
